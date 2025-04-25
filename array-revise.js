0b0f746d5493a27510079846d51e9aac80671733
shubhamraicopythat <quicktalkme@gmail.com>
2025-04-25 20:24:54 +0530

    array revesion

--- array-revise.js
+++ array-revise.js
@@ -1,3 +1,8 @@
-let arr = ["shubham ", "rakesh ", "mahesh ", "Ram "];
-console.log(arr.length);
-console.log(arr);
+let arr1 = ["shubham ", "rakesh ", "mahesh ", "Ram "];
+let arr2 = ["One", "Two", "Three ", "Four"];
+console.log(arr1.length);
+console.log(arr1);
+console.log(arr1.indexOf[1]);
+console.log(arr1 + arr2);
+arr1[1] = "seema";
+console.log(arr1);
